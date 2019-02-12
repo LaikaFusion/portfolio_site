ec73e4c003248688d46e91950ecbd35680cde994
LaikaFusion <newrad4@gmail.com>
2019-02-12 17:11:48 -0500

    added page checking

--- portfolio/src/components/Blog/PostList.jsx
+++ portfolio/src/components/Blog/PostList.jsx
@@ -15,13 +15,59 @@ export default class PostList extends Component {
         title: "Test",
         summary:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non turpis ante. Aliquam commodo, diam in commodo malesuada, enim ante molestie lacus, mollis congue quam turpis id magna. Donec quis metus quis lorem volutpat pellentesque. Nulla bibendum pellentesque lectus non commodo. Nam vitae ullamcorper massa. Integer at porttitor nunc. Aliquam vitae arcu fermentum, eleifend nunc ac, pharetra nisl. Vestibulum in venenatis lorem. Morbi accumsan, tellus sit amet porttitor sollicitudin, tellus neque imperdiet sapien, sed ultrices urna sapien vitae est.",
-        url: "test"
+        url: "test1"
       },
       {
         title: "Test",
         summary:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non turpis ante. Aliquam commodo, diam in commodo malesuada, enim ante molestie lacus, mollis congue quam turpis id magna. Donec quis metus quis lorem volutpat pellentesque. Nulla bibendum pellentesque lectus non commodo. Nam vitae ullamcorper massa. Integer at porttitor nunc. Aliquam vitae arcu fermentum, eleifend nunc ac, pharetra nisl. Vestibulum in venenatis lorem. Morbi accumsan, tellus sit amet porttitor sollicitudin, tellus neque imperdiet sapien, sed ultrices urna sapien vitae est.",
         url: "test"
+      },{
+        title: "Test",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non turpis ante. Aliquam commodo, diam in commodo malesuada, enim ante molestie lacus, mollis congue quam turpis id magna. Donec quis metus quis lorem volutpat pellentesque. Nulla bibendum pellentesque lectus non commodo. Nam vitae ullamcorper massa. Integer at porttitor nunc. Aliquam vitae arcu fermentum, eleifend nunc ac, pharetra nisl. Vestibulum in venenatis lorem. Morbi accumsan, tellus sit amet porttitor sollicitudin, tellus neque imperdiet sapien, sed ultrices urna sapien vitae est.",
+        url: "test"
+      },{
+        title: "Test",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non turpis ante. Aliquam commodo, diam in commodo malesuada, enim ante molestie lacus, mollis congue quam turpis id magna. Donec quis metus quis lorem volutpat pellentesque. Nulla bibendum pellentesque lectus non commodo. Nam vitae ullamcorper massa. Integer at porttitor nunc. Aliquam vitae arcu fermentum, eleifend nunc ac, pharetra nisl. Vestibulum in venenatis lorem. Morbi accumsan, tellus sit amet porttitor sollicitudin, tellus neque imperdiet sapien, sed ultrices urna sapien vitae est.",
+        url: "test"
+      },{
+        title: "Test",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non turpis ante. Aliquam commodo, diam in commodo malesuada, enim ante molestie lacus, mollis congue quam turpis id magna. Donec quis metus quis lorem volutpat pellentesque. Nulla bibendum pellentesque lectus non commodo. Nam vitae ullamcorper massa. Integer at porttitor nunc. Aliquam vitae arcu fermentum, eleifend nunc ac, pharetra nisl. Vestibulum in venenatis lorem. Morbi accumsan, tellus sit amet porttitor sollicitudin, tellus neque imperdiet sapien, sed ultrices urna sapien vitae est.",
+        url: "test"
+      },{
+        title: "Test",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non turpis ante. Aliquam commodo, diam in commodo malesuada, enim ante molestie lacus, mollis congue quam turpis id magna. Donec quis metus quis lorem volutpat pellentesque. Nulla bibendum pellentesque lectus non commodo. Nam vitae ullamcorper massa. Integer at porttitor nunc. Aliquam vitae arcu fermentum, eleifend nunc ac, pharetra nisl. Vestibulum in venenatis lorem. Morbi accumsan, tellus sit amet porttitor sollicitudin, tellus neque imperdiet sapien, sed ultrices urna sapien vitae est.",
+        url: "test"
+      },{
+        title: "Test",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non turpis ante. Aliquam commodo, diam in commodo malesuada, enim ante molestie lacus, mollis congue quam turpis id magna. Donec quis metus quis lorem volutpat pellentesque. Nulla bibendum pellentesque lectus non commodo. Nam vitae ullamcorper massa. Integer at porttitor nunc. Aliquam vitae arcu fermentum, eleifend nunc ac, pharetra nisl. Vestibulum in venenatis lorem. Morbi accumsan, tellus sit amet porttitor sollicitudin, tellus neque imperdiet sapien, sed ultrices urna sapien vitae est.",
+        url: "test7"
+      },{
+        title: "Test",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non turpis ante. Aliquam commodo, diam in commodo malesuada, enim ante molestie lacus, mollis congue quam turpis id magna. Donec quis metus quis lorem volutpat pellentesque. Nulla bibendum pellentesque lectus non commodo. Nam vitae ullamcorper massa. Integer at porttitor nunc. Aliquam vitae arcu fermentum, eleifend nunc ac, pharetra nisl. Vestibulum in venenatis lorem. Morbi accumsan, tellus sit amet porttitor sollicitudin, tellus neque imperdiet sapien, sed ultrices urna sapien vitae est.",
+        url: "test8"
+      },{
+        title: "Test",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non turpis ante. Aliquam commodo, diam in commodo malesuada, enim ante molestie lacus, mollis congue quam turpis id magna. Donec quis metus quis lorem volutpat pellentesque. Nulla bibendum pellentesque lectus non commodo. Nam vitae ullamcorper massa. Integer at porttitor nunc. Aliquam vitae arcu fermentum, eleifend nunc ac, pharetra nisl. Vestibulum in venenatis lorem. Morbi accumsan, tellus sit amet porttitor sollicitudin, tellus neque imperdiet sapien, sed ultrices urna sapien vitae est.",
+        url: "test"
+      },{
+        title: "Test9",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non turpis ante. Aliquam commodo, diam in commodo malesuada, enim ante molestie lacus, mollis congue quam turpis id magna. Donec quis metus quis lorem volutpat pellentesque. Nulla bibendum pellentesque lectus non commodo. Nam vitae ullamcorper massa. Integer at porttitor nunc. Aliquam vitae arcu fermentum, eleifend nunc ac, pharetra nisl. Vestibulum in venenatis lorem. Morbi accumsan, tellus sit amet porttitor sollicitudin, tellus neque imperdiet sapien, sed ultrices urna sapien vitae est.",
+        url: "test10"
+      },
+      {
+        title: "Test",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non turpis ante. Aliquam commodo, diam in commodo malesuada, enim ante molestie lacus, mollis congue quam turpis id magna. Donec quis metus quis lorem volutpat pellentesque. Nulla bibendum pellentesque lectus non commodo. Nam vitae ullamcorper massa. Integer at porttitor nunc. Aliquam vitae arcu fermentum, eleifend nunc ac, pharetra nisl. Vestibulum in venenatis lorem. Morbi accumsan, tellus sit amet porttitor sollicitudin, tellus neque imperdiet sapien, sed ultrices urna sapien vitae est.",
+        url: "test11"
       },
       {
         title: "Test",
@@ -111,15 +157,29 @@ export default class PostList extends Component {
 
     this.state = {
       curPag: 1,
-      postList: this.postsObj.slice(0, 9)
+      postList: this.postsObj.slice(0, 10)
     };
   }
   changePage = (direction)=>{
     if(direction === "left"){
       const newPage = this.state.curPag -1;
+      if(newPage === 0){
+        return
+      }
       this.setState({
         curPag: newPage,
-        postList: this.postsObj.slice(this.state.curPag - (newPage * 10))
+        postList: this.postsObj.slice((newPage * 10) - 10, (newPage * 10))
+        
+      })
+    }
+    else{
+      const newPage = this.state.curPag + 1;
+      if(newPage > Math.ceil(this.postsObj.length/10)){
+        return
+      }
+      this.setState({
+        curPag: newPage,
+        postList: this.postsObj.slice((this.state.curPag * 10), (newPage * 10))
         
       })
     }
@@ -132,8 +192,8 @@ export default class PostList extends Component {
         })}
         {this.postsObj.length > 10 ? (
           <div className="paginator">
-            <FontAwesomeIcon icon={faArrowLeft}/>
-            {this.state.curPag}<FontAwesomeIcon icon={faArrowRight}/>
+            <FontAwesomeIcon onClick={()=>this.changePage('left')} icon={faArrowLeft}/>
+            {this.state.curPag}<FontAwesomeIcon onClick={()=>this.changePage('right')} icon={faArrowRight}/>
           </div>
         ) : (
           ""
